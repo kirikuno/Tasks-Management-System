2a5ada115bf78ac4a2318106ce4164ae5cef9d23
shivalcrow <58440335+shivalcrow@users.noreply.github.com>
2020-10-09 15:21:31 +0700

    add model project

---
+++ src/main/java/com/java/model/Project.java
@@ -0,0 +1,41 @@
+package com.java.model;
+
+import java.util.Date;
+
+public class Project 
+{
+private int project_id;
+private String project_name;
+private String project_description;
+private Date due_date;
+public int getProject_id() {
+	return project_id;
+}
+public void setProject_id(int project_id) {
+	this.project_id = project_id;
+}
+public String getProject_name() {
+	return project_name;
+}
+public void setProject_name(String project_name) {
+	this.project_name = project_name;
+}
+public String getProject_description() {
+	return project_description;
+}
+public void setProject_description(String project_description) {
+	this.project_description = project_description;
+}
+public Date getDue_date() {
+	return due_date;
+}
+public void setDue_date(Date due_date) {
+	this.due_date = due_date;
+}
+@Override
+public String toString() {
+	return "Project [project_id=" + project_id + ", project_name=" + project_name + ", project_description="
+			+ project_description + "]";
+}
+
+}
